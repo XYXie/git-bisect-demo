d0dead6a71b9040b6dd4d2ea6619d42afac31e46
Alexander Groß <agross@therightstuff.de>
2010-08-25 13:24:47 +0200

    Adding the subtraction feature

--- source/Calculator/CalculatorSpecs.cs
+++ source/Calculator/CalculatorSpecs.cs
@@ -15,4 +15,18 @@ namespace Calculator
 		It should_add_both_operands =
 			() => Result.ShouldEqual(42);
 	}
+	
+	[Subject(typeof(Calculator))]
+	public class When_two_numbers_are_subtracted
+	{
+		static Calculator Calculator;
+		static int Result;
+
+		Establish context = () => { Calculator = new Calculator(); };
+
+		Because of = () => { Result = Calculator.Subtract(40, 2); };
+
+		It should_subtract_the_right_operand_from_the_left_operand =
+			() => Result.ShouldEqual(38);
+	}
 }
