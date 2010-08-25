1e9a44f17dc9568905da31f2a1bd7fa6c8cec6bc
Alexander Groß <agross@therightstuff.de>
2010-08-25 13:27:50 +0200

    Adding the division feature

--- source/Calculator/CalculatorSpecs.cs
+++ source/Calculator/CalculatorSpecs.cs
@@ -15,4 +15,18 @@ namespace Calculator
 		It should_subtract_the_right_operand_from_the_left_operand =
 			() => Result.ShouldEqual(38);
 	}
+	
+	[Subject(typeof(Calculator))]
+	public class When_two_numbers_are_divided
+	{
+		static Calculator Calculator;
+		static int Result;
+
+		Establish context = () => { Calculator = new Calculator(); };
+
+		Because of = () => { Result = Calculator.Divide(40, 2); };
+
+		It should_divide_the_left_operand_with_the_right_operand =
+			() => Result.ShouldEqual(20);
+	}
 }
